3647ae7627fe408a651079048953871d16bb8c73
Lucy! <89477983+ConsistentlyNervous@users.noreply.github.com>
2025-01-28 23:37:41 -0500

    finished first pass of start screen

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,12 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class StartMenu : MonoBehaviour
 {
     [SerializeField] private Transform zoomInPos;
     [SerializeField] private Transform zoomOutPos;
+    [SerializeField] private Transform zoomComputerPos;
     [SerializeField] private CameraUtils cameraMovement;
-    [SerializeField] private ClickableObject[] menuOptions;
+    [SerializeField] private GameObject interactionText;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private EventTrigger[] menuButtons;
 
     private bool zoomedIn = false;
 
@@ -14,7 +21,9 @@ public class StartMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        foreach (EventTrigger button in menuButtons) {
+            button.enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -24,20 +33,30 @@ public class StartMenu : MonoBehaviour
         {
             Debug.Log("Zooming!");
             zoomedIn = true;
-            StartCoroutine(cameraMovement.ZoomCoroutine(zoomInPos));
+            interactionText.SetActive(false);
+            StartCoroutine(cameraMovement.ZoomCoroutine(zoomInPos, () => {foreach (EventTrigger button in menuButtons) {button.enabled = true;}}));
         }
     }
 
     public void StartGame() {
         Debug.Log("Starting game");
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        StartCoroutine(cameraMovement.ZoomCoroutine(zoomOutPos, () => {SceneManager.LoadScene("SceneTransitionTest");}));
     }
 
     public void OpenOptions() {
         Debug.Log("opening options");
+        optionsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        StartCoroutine(cameraMovement.ZoomCoroutine(zoomComputerPos));
     }
 
     public void OpenCredits() {
         Debug.Log("opening credits");
+        creditsPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        StartCoroutine(cameraMovement.ZoomCoroutine(zoomComputerPos));
     }
 
     public void QuitGame() {
