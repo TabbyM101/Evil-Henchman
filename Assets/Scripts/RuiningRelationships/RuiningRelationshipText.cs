7d3ed089bb70ba0cedfb677dcbed68970df59eb8
lapomme-auguste <lapommeauguste@gmail.com>
2025-02-24 10:10:07 -0500

    Ruining Relationship V1

---
+++ Assets/Scripts/RuiningRelationships/RuiningRelationshipText.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RuiningRelationshipText", menuName = "ScriptableObjects/RuiningRelationshipText")]
+public class RuiningRelationshipText : ScriptableObject
+{
+    public string From1;
+    public string To1Good;
+    public string To1Bad;
+
+    public string From2Good;
+    public string From2Bad;
+    public string To2Good;
+    public string To2Bad;
+
+    public string From3Good;
+    public string From3Bad;
+    public string To3Good;
+    public string To3Bad;
+
+    public string FromOverallSuccess;
+    public string FromOverallFailure;
+    
+}
---
+++ Assets/Scripts/RuiningRelationships/RuiningRelationships.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class RuiningRelationships : MonoBehaviour, IMinigame
+{
+    private bool gameRunning = false;
+    private bool isTyping = false;
+
+    [SerializeField] private TextMeshProUGUI email;
+    [SerializeField] private TextMeshProUGUI receiveEmail;
+    [SerializeField] private Button sendButton;
+    [SerializeField] private RuiningRelationshipText source;
+    [SerializeField] private Button choice1;
+    [SerializeField] private Button choice2;
+
+    private string currentParagraph;
+    private int currentIndex;
+    private int currentRound = 0;
+    private int badCounter = 0;
+    private bool lastPicked;
+
+    public void Start()
+    {
+        StartMinigame();
+    }
+
+    public void StartMinigame()
+    {
+        gameRunning = true;
+        currentRound = 1;
+        badCounter = 0;
+        receiveEmail.text = source.From1;
+        choice1.GetComponentInChildren<TextMeshProUGUI>().text = source.To1Good;
+        choice2.GetComponentInChildren<TextMeshProUGUI>().text = source.To1Bad;
+        // choice1.onClick.AddListener(() => SelectParagraph(choice1.GetComponentInChildren<TextMeshProUGUI>().text, true));
+        // choice2.onClick.AddListener(() => SelectParagraph(choice2.GetComponentInChildren<TextMeshProUGUI>().text, false));
+        sendButton.onClick.AddListener(SendParagraph);
+        sendButton.interactable = false;
+        sendButton.gameObject.SetActive(false);
+        email.gameObject.SetActive(false);
+    }
+
+    void Update() {
+        if (gameRunning) {
+            if (isTyping && Input.anyKey) {
+                if (currentIndex < currentParagraph.Length) {
+                    email.text += currentParagraph[currentIndex];
+                    currentIndex++;
+                } else {
+                    isTyping = false;
+                    sendButton.interactable = true;
+                }
+            }
+        }
+    }
+
+    public void SelectParagraph(bool isGood) {
+        if (currentRound == 1) {
+            if (isGood) {
+                currentParagraph = source.To1Good;
+            } else {
+                currentParagraph = source.To1Bad;
+            }
+        } else if (currentRound == 2) {
+            if (isGood) {
+                currentParagraph = source.To2Good;
+            } else {
+                currentParagraph = source.To2Bad;
+            }
+            
+        } else if (currentRound == 3) {
+            if (isGood) {
+                currentParagraph = source.To3Good;
+            } else {
+                currentParagraph = source.To3Bad;
+            }
+        }
+        isTyping = true;
+        choice1.gameObject.SetActive(false);
+        choice2.gameObject.SetActive(false);
+        sendButton.gameObject.SetActive(true);
+        sendButton.interactable = false;
+        email.gameObject.SetActive(true);
+        email.text = "";
+        receiveEmail.gameObject.SetActive(false);
+        lastPicked = isGood;
+        if (!isGood) {
+            badCounter++;
+        }
+    }
+
+    private void SendParagraph() {
+        currentRound++;
+        currentIndex = 0;
+        if (currentRound == 4) {
+            if (badCounter >= 2) {
+                MinigameWon();
+            } else {
+                MinigameLost();
+            }
+        } else {
+            isTyping = false;
+            if (currentRound == 2) {
+                choice1.GetComponentInChildren<TextMeshProUGUI>().text = source.To2Good;
+                choice2.GetComponentInChildren<TextMeshProUGUI>().text = source.To2Bad;
+                if (lastPicked) {
+                    receiveEmail.text = source.From2Good;
+                } else {
+                    receiveEmail.text = source.From2Bad;
+                }
+            } else if (currentRound == 3) {
+                choice1.GetComponentInChildren<TextMeshProUGUI>().text = source.To3Good;
+                choice2.GetComponentInChildren<TextMeshProUGUI>().text = source.To3Bad;
+                if (lastPicked) {
+                    receiveEmail.text = source.From3Good;
+                } else {
+                    receiveEmail.text = source.From3Bad;
+                }
+            }
+            email.gameObject.SetActive(false);
+            receiveEmail.gameObject.SetActive(true);
+            choice1.gameObject.SetActive(true);
+            choice2.gameObject.SetActive(true);
+            sendButton.gameObject.SetActive(false);
+        }
+    }
+
+    private void MinigameWon()
+    {
+        email.gameObject.SetActive(false);
+        receiveEmail.gameObject.SetActive(true);
+        choice1.gameObject.SetActive(false);
+        choice2.gameObject.SetActive(false);
+        sendButton.gameObject.SetActive(false);
+        receiveEmail.text = source.FromOverallSuccess;
+        Debug.Log("Won Ruining Relationships!");
+        MinigameManager.Current.EndMinigame(CompletionState.Completed);
+    }
+
+    private void MinigameLost()
+    {
+        email.gameObject.SetActive(false);
+        receiveEmail.gameObject.SetActive(true);
+        choice1.gameObject.SetActive(false);
+        choice2.gameObject.SetActive(false);
+        sendButton.gameObject.SetActive(false);
+        receiveEmail.text = source.FromOverallFailure;
+        Debug.Log("Picked incorrect color! Closing the game...");
+        MinigameManager.Current.EndMinigame(CompletionState.Failed);
+        // StartMinigame(); this line would restart the minigame, but it is set to end the minigame via the manager above
+    }
+}
